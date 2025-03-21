43050be21cbc27fa4d0abdd17f3261219de09e76
Nicholas Lui <nicholas.lui1@ucalgary.ca>
2025-03-21 11:17:07 -0600

    Added prettify method.

--- Lab_8/web_analyzer.py
+++ Lab_8/web_analyzer.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+
 url = "https://en.wikipedia.org/wiki/University_of_Calgary"
+
 try:
     response = requests.get(url)
     response.raise_for_status() # Ensures the request was successful
@@ -8,3 +10,5 @@ try:
     print(f"Successfully fetched content from {url}")
 except Exception as e:
     print(f"Error fetching content: {e}")
+
+print(soup.prettify())
