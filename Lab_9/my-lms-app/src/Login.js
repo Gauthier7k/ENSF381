aebb4acf06364838863396d87a4b7ccc33b30a47
Nicholas Lui <nicholas.lui1@ucalgary.ca>
2025-03-28 11:34:12 -0600

    Starting files.

---
+++ Lab_9/my-lms-app/src/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    const backendEndpoint = 'http://127.0.0.1:5000/validate_login';
+    try {
+        const response = await fetch(backendEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'username': username, 'password': password}),
+        });
+
+        if (response.ok) {
+            console.log('SUCCESS');
+        } else {
+            console.error("ERROR");
+        }
+    } catch (error) {
+        console.error("Error", error);
+    }
+    
+};
+
+
+function Login() {
+    return(
+
+    );
+}
